47768165426a47faf4749a5ca066d8315ed62afd
LukeOB95 <luke.oboyle@gmail.com>
2025-07-03 13:37:44 -0400

    Changes made on July 4th, 2025.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const isbnNumber = require("./booksdb.js").isbn;
 
 
 public_users.post("/register", (req,res) => {
@@ -29,17 +30,71 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   
-    const author = req.params.author;
-
-    let filtered_authors = books.books.filter((books) => books.author === author)
+    // Author parameter
+    const authorParameter = req.params.author.toLowerCase();
     
-    res.send(books[author]);
+    // Array of matching books
+    const matchingBooks = [];
+
+    // Loop through book array
+    for (let key in books)
+    {
+        // If the author defined in the URL matches that of one in the list
+        if (books[key].author.toLowerCase() === authorParameter)
+        {
+            // Retrieve details
+            matchingBooks.push({isbnNumber, key, ...books[key]});
+        }
+    }
+
+    // Error handling and notification of success
+    
+    // If the array is greater than zero
+    if (matchingBooks.length > 0)
+    {
+        // Return matching book
+        return res.status(200).json(matchingBooks);
+    }
+
+    // Else if it is not
+    else{
+        // Return error message
+        return res.status(404).json({message: `No books found by author '${req.params.author}' .`});
+    }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+    // Title parameter
+    const titleParam = req.params.title.toLowerCase();
+
+    // Array of titles
+    const bookTitles = [];
+
+    // Loop through the array of books
+    for (let key in books)
+    {
+        // If the title matches that of any given book
+        if (books[key].title.toLowerCase() === titleParam)
+        {
+            // Retrieve all details
+            bookTitles.push({isbnNumber, key, ...books[key]});
+        }
+    }
+
+    // Error handling and notification of success
+    if (bookTitles.length > 0)
+    {
+        // Return book's details
+        return res.status(200).json(bookTitles);
+    }
+
+    // Else if no such book exists in the array
+    else {
+        // Return error message
+        return res.status(404).json({message: `Could not find any books with the title '${req.params.title}' . `});
+    }
 });
 
 //  Get book review
